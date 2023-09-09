aae0c984e0794aac6aae7a8baa32eaab0e323e1b
Jian Liu <823957468@qq.com>
2023-09-09 11:56:59 +0800

    'Community'

---
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nowcoder.community;
+import com.nowcoder.community.dao.AlphaDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("nnihao ");
+	}
+
+}
